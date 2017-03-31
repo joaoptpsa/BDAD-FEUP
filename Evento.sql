21eb29df54625f7fa42afe13c0b561228d525796
Bernardo Barbosa <up201503477@fe.up.pt>
2017-04-01 00:42:03 +0100

    Add Evento sql

---
+++ Evento.sql
@@ -0,0 +1,24 @@
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (1, 25, 12, 257, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (2, 76, 12, 257, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (3, 74, 12, 258, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (4, 80, 12, 256, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (5, 71, 12, 264, 1);
+
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (6, 69, 4, 72, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (7, 6, 4, 74, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (8, 65, 4, 76, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (9, 18, 4, 76, 1);
+insert into Evento (id, minuto, idEquipa, idJogador, idJogo) values (10, 51, 4, 78, 1);
+
+
+insert into Golo (idEvento) values (1);
+insert into Golo (idEvento) values (2);
+insert into Golo (idEvento) values (3);
+insert into Golo (idEvento) values (4);
+insert into Amarelo (idEvento) values (5);
+
+insert into Golo (idEvento) values (6);
+insert into Golo (idEvento) values (7);
+insert into Golo (idEvento) values (8);
+insert into Golo (idEvento) values (9);
+insert into Vermelho (idEvento) values (10);
