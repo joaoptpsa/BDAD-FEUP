c813313362c3747d75bbad1040b131a0a6bd9957
Bernardo Barbosa <up201503477@fe.up.pt>
2017-03-31 23:24:13 +0100

    Add Convocado and Participou sql

---
+++ Convocado.sql
@@ -0,0 +1,40 @@
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (254, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (255, 1, '0', '45');
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (256, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (257, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (258, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (259, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (260, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (261, 1, '0', '75');
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (262, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (263, 1, '0', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (264, 1, '0', '90');
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (265, 1, '90', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (266, 1, '45', NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (267, 1, NULL,NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (268, 1, NULL,NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (269, 1, NULL,NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (270, 1, NULL,NULL);
+insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (271, 1, '75', NULL);
+
+
+
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (70, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (71, 1, '0', '60');
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (72, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (73, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (74, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (75, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (76, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (77, 1, '0', '40');
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (78, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (79, 1, '0', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (80, 1, '0', '93');
+
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (81, 1, '93', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (82, 1, '60', NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (83, 1, NULL,NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (84, 1, NULL,NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (85, 1, NULL,NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (86, 1, NULL,NULL);
+  insert into Convocado (idJogador, idJogo, minutoEntrada, minutoSaida) values (87, 1, '40', NULL);
