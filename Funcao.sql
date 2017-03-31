0160e9f9203ed86fb3dae3557aea187e9aa95b82
Bernardo Barbosa <up201503477@fe.up.pt>
2017-03-31 12:33:14 +0100

    Add database and some sql class

---
+++ Funcao.sql
@@ -0,0 +1,7 @@
+insert into Funcao (id, nome) values (1, 'Agueiro');
+insert into Funcao (id, nome) values (2, 'Diretor');
+insert into Funcao (id, nome) values (3, 'Agueiro');
+insert into Funcao (id, nome) values (4, 'Agueiro');
+insert into Funcao (id, nome) values (5, 'Roupeiro');
+insert into Funcao (id, nome) values (6, 'Agueiro');
+insert into Funcao (id, nome) values (7, 'Agueiro');
