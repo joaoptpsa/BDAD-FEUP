810558167349e73cd97afd9edcb8cf64a9498d89
João Sá <up201506252@fe.up.pt>
2017-03-31 03:02:27 +0100

    Added support for foreign with pragma
    Started adding Constraints and foreign keys to classes

--- criar.sql
+++ criar.sql
@@ -1,4 +1,12 @@
-/*FALTA AS KEYS*/
+/*Formatting Output*/
+.header on
+.mode column
+.timer on
+
+/*Allow Foreign Keys*/
+PRAGMA foreign_keys = ON;
+
+/*MUDAR AS DATAS PARA STRING ?*/
 
 DROP TABLE IF EXISTS Epoca;
 DROP TABLE IF EXISTS Jornada;
@@ -15,71 +23,89 @@ DROP TABLE IF EXISTS Estadio;
 DROP TABLE IF EXISTS Pais;
 DROP TABLE IF EXISTS Cidade;
 DROP TABLE IF EXISTS Golo;
+DROP TABLE IF EXISTS Amarelo;
+DROP TABLE IF EXISTS Vermelho;
 
 
 CREATE TABLE Epoca (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	dataInicio REAL, -- para guardar o numero de dias 
 	dataFim REAL -- para guardar o numero de dias 
 );
 
 CREATE TABLE Jornada (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	dataInicio REAL, 
 	dataFim REAL
 );
 
 CREATE TABLE Jogo (
-	id INTEGER,
-	data REAL,
-	golosCasa INTEGER,
-	golosFora INTEGER 
+	id 				INTEGER 	PRIMARY KEY,
+	data 			REAL		NOT NULL,
+	golosCasa 		INTEGER		CHECK(golosCasa>=0),
+	golosFora 		INTEGER		CHECK(golosFora>=0),
+	idEquipaCasa	INTEGER		NOT NULL,
+	idEquipaFora	INTEGER		NOT NULL,
+	idEstadio		INTEGER		NOT NULL,
+	idArbitro		INTEGER		NOT NULL,
+	idJornada		INTEGER		NOT NULL,
+		FOREIGN KEY (idEquipaCasa) REFERENCES Equipa,
+		FOREIGN KEY (idEquipaFora) REFERENCES Equipa,
+		FOREIGN KEY (idEstadio) REFERENCES Estadio,
+		FOREIGN KEY (idArbitro) REFERENCES Arbitro,
+		FOREIGN KEY (idJornada) REFERENCES Jornada
 );
 
 CREATE TABLE Pessoa (
-	id INTEGER,
-	nome TEXT,
-	dataNascimento REAL, 
-	altura INTEGER, -- cm
-	peso INTEGER --kg
+	id 				INTEGER 	PRIMARY KEY,
+	nome 			TEXT 		NOT NULL,
+	dataNascimento 	REAL		NOT NULL, 
+	altura 			INTEGER		CHECK(altura>0), -- cm
+	peso 			INTEGER		CHECK(peso>0)--kg
 );
 
 CREATE TABLE Jogador (
+	idPessoa		INTEGER		NOT NULL,
 	posicaoPref TEXT,
-	pePref TEXT
+	pePref TEXT,
+		FOREIGN KEY (idPessoa) REFERENCES Pessoa
 );
 
 CREATE TABLE Staff (
+	idPessoa		INTEGER		NOT NULL,
+		FOREIGN KEY (idPessoa) REFERENCES Pessoa
 );
 
 CREATE TABLE Arbitro (
+	idPessoa		INTEGER		NOT NULL,
+		FOREIGN KEY (idPessoa) REFERENCES Pessoa
 );
 
 CREATE TABLE Contrato (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	dataInicio REAL, 
 	dataFim REAL
 );
 
 CREATE TABLE Funcao (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	nome TEXT
 );
 
 CREATE TABLE Equipamento (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	corCamisola TEXT,
 	corCalcoes TEXT
 );
 
 CREATE TABLE Equipa (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	nome TEXT,
 	dataFundacao REAL 
 );
 
 CREATE TABLE Estadio (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	nome TEXT,
 	dataAbertura REAL, 
 	morada TEXT,
@@ -87,12 +113,12 @@ CREATE TABLE Estadio (
 );
 
 CREATE TABLE Pais (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	nome TEXT
 );
 
 CREATE TABLE Cidade (
-	id INTEGER,
+	id 				INTEGER 	PRIMARY KEY,
 	nome TEXT
 );
 
--- povoar.sql
+++ povoar.sql
@@ -1,1 +1,4 @@
-PRAGMA foreign_keys = ON;
+
+
+INSERT INTO Pessoa (id,nome,dataNascimento,altura,peso)
+VALUES (1, 'Cristiano Ronaldo', '1985-02-05', 185, 85);
