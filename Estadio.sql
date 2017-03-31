53bba99d714e4e7946d4d240a15a876ef04158e0
Bernardo Barbosa <up201503477@fe.up.pt>
2017-03-31 16:06:51 +0100

    Add Epoca, Estadio, Pessoa and Funcao sql

---
+++ Estadio.sql
@@ -0,0 +1,18 @@
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (1, 'Anfield Road', 1892, 'Liverpool', 45276);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (2, 'Britannia Stadium', 1997, 'Stoke', 27740);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (3, 'Goodison Park', 1892, 'Liverpool', 40157);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (4, 'KC Stadium', 2002, 'Hull', 25586);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (5, 'King Power Stadium', 2002, 'Leicester', 32262);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (6, 'Old Trafford', 1910, 'Manchester', 75731);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (7, 'Olympic Stadium London', 2011, 'London', 60000);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (8, 'Riverside Stadium', 1995, 'Middlesbrough', 34742);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (9, 'Selhurst Park', 1924, 'London', 26309);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (10, 'St Mary''s Stadium', 2001, 'Southampton', 32363);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (11, 'Stadium of Light', 1997, 'Sunderland', 49000);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (12, 'Stamford Bridge', 1876, 'London', 41837);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (13, 'The Emirates', 2006, 'London', 60338);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (14, 'The Etihad', 2003, 'Manchester', 47805);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (15, 'The Hawthorns', 1900, 'West Bromwich', 27000);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (16, 'Turf Moor', 1883, 'Burnley', 21940);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (17, 'Vitality Stadium', 1910, 'Bournemouth', 12000);
+insert into Estadio (id, nome, dataAbertura, morada, lotacao) values (18, 'White Hart Lane', 1899, 'London', 36230);
--- Funcao.sql
+++ Funcao.sql
@@ -1,7 +1,7 @@
-insert into Funcao (id, nome) values (1, 'Agueiro');
-insert into Funcao (id, nome) values (2, 'Diretor');
-insert into Funcao (id, nome) values (3, 'Agueiro');
-insert into Funcao (id, nome) values (4, 'Agueiro');
+insert into Funcao (id, nome) values (1, 'Treinador Principal');
+insert into Funcao (id, nome) values (2, 'Treinador-Adjunto');
+insert into Funcao (id, nome) values (3, 'Preparador Físico');
+insert into Funcao (id, nome) values (4, 'Massagista');
 insert into Funcao (id, nome) values (5, 'Roupeiro');
 insert into Funcao (id, nome) values (6, 'Agueiro');
-insert into Funcao (id, nome) values (7, 'Agueiro');
+insert into Funcao (id, nome) values (7, 'Diretor');
