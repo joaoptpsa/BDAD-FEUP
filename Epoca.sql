8a2ee90d0dd71fc2c6650c0a7e0e8f2555b961eb
Bernardo Barbosa <up201503477@fe.up.pt>
2017-03-31 23:36:57 +0100

    Update Epoca sql

--- Epoca.sql
+++ Epoca.sql
@@ -2,4 +2,4 @@ insert into Epoca (id, dataInicio, dataFim) values (1, '2016-08-13', '2017-04-03
 insert into Epoca (id, dataInicio, dataFim) values (2, '2017-08-13', '2018-04-03');
 insert into Epoca (id, dataInicio, dataFim) values (3, '2018-08-13', '2019-04-03');
 insert into Epoca (id, dataInicio, dataFim) values (4, '2019-08-13', '2020-04-03');
-insert into Epoca (id, dataInicio, dataFim) values (1, '2020-08-13', '2021-04-03');
+insert into Epoca (id, dataInicio, dataFim) values (5, '2020-08-13', '2021-04-03');
