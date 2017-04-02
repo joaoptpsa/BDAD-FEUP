28cab4d7f2df8d5121102149009ce70744fe2994
João Sá <up201506252@fe.up.pt>
2017-04-02 05:31:45 +0100

    Changed id var name to be more specific

--- Funcao.sql
+++ Funcao.sql
@@ -1,7 +1,7 @@
-insert into Funcao (id, nome) values (1, 'Treinador Principal');
-insert into Funcao (id, nome) values (2, 'Treinador-Adjunto');
-insert into Funcao (id, nome) values (3, 'Preparador Físico');
-insert into Funcao (id, nome) values (4, 'Massagista');
-insert into Funcao (id, nome) values (5, 'Roupeiro');
-insert into Funcao (id, nome) values (6, 'Agueiro');
-insert into Funcao (id, nome) values (7, 'Diretor');
+insert into Funcao (idFuncao, nome) values (1, 'Treinador Principal');
+insert into Funcao (idFuncao, nome) values (2, 'Treinador-Adjunto');
+insert into Funcao (idFuncao, nome) values (3, 'Preparador Físico');
+insert into Funcao (idFuncao, nome) values (4, 'Massagista');
+insert into Funcao (idFuncao, nome) values (5, 'Roupeiro');
+insert into Funcao (idFuncao, nome) values (6, 'Agueiro');
+insert into Funcao (idFuncao, nome) values (7, 'Diretor');
