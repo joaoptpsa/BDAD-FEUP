cfc82151404a0e2b4417478c71c3768039834eae
João Sá <up201506252@fe.up.pt>
2017-05-22 18:50:36 +0100

    Most represented countries

--- int3.sql
+++ int3.sql
@@ -2,4 +2,13 @@
 .headers on
 .nullvalue NULL
 
-/*INTERROGACAO 3 - */
+/*INTERROGACAO 3 - Paises mais representados na liga*/
+
+SELECT Pais.nome, count(Pais.nome)
+FROM Jogador INNER JOIN Pessoa
+ON Jogador.idPessoa=Pessoa.idPessoa
+INNER JOIN Cidade
+ON Pessoa.idCidadeNasc = Cidade.idCidade
+INNER JOIN Pais
+ON Cidade.idPais=Pais.idPais
+GROUP BY Pais.nome;
