ed1d1808edcc855c176ba20e27f1ab3e6906b336
João Sá <up201506252@fe.up.pt>
2017-05-22 18:32:04 +0100

    Best all time scorers int

--- int2.sql
+++ int2.sql
@@ -4,6 +4,12 @@
 
 /*INTERROGACAO 2 - Melhores marcardores*/
 
-SELECT idJogador FROM Evento
-INTERSECT
-SELECT idPessoa FROM Jogador;
+
+SELECT Pessoa.nome, count(Pessoa.nome)
+FROM Golo INNER JOIN Evento
+ON Golo.idEvento = Evento.idEvento
+INNER JOIN Jogador
+ON Jogador.idPessoa = Evento.idJogador
+INNER JOIN Pessoa
+ON Pessoa.idPessoa=Jogador.idPessoa
+GROUP BY Pessoa.nome;
