6ca2e94c4208f171a3460ffe3e6e607a5c31e062
Bernardo Barbosa <up201503477@fe.up.pt>
2017-05-23 23:46:49 +0100

    int9 - avg age of teams

--- int9.sql
+++ int9.sql
@@ -5,14 +5,9 @@
 .width 10 20
 /*INTERROGACAO 9 - Média de Idades por Equipa*/
 
-/*
-SELECT  ContratoJogador.*, AVG(strftime('%Y', 'now')-strftime ('%Y', Pessoa.dataNascimento)) as Idade
-FROM ContratoJogador, Pessoa
-WHERE idEquipa=4 AND idJogador = idPessoa;
- */
 
-
--- SELECT idEquipa as ID,nome as Nome FROM Equipa
-SELECT Equipa.idEquipa,Equipa.nome, AVG(strftime('%Y', 'now')-strftime ('%Y', Pessoa.dataNascimento)) as Média_Idades
-FROM Equipa,ContratoJogador, Pessoa
-WHERE idEquipa=4 AND idJogador = idPessoa;
+SELECT Equipa.nome, printf("%.2f",AVG(strftime('%Y', 'now')-strftime ('%Y', Pessoa.dataNascimento))) as Idade
+FROM ContratoJogador, Pessoa INNER JOIN Equipa
+WHERE ContratoJogador.idJogador = Pessoa.idPessoa AND ContratoJogador.idEquipa = Equipa.idEquipa
+GROUP BY Equipa.idEquipa
+ORDER BY Idade ASC;
