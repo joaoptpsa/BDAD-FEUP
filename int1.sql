646a737cd348e925341c2f4583628c8a2137cd3a
João Sá <up201506252@fe.up.pt>
2017-05-23 21:38:40 +0100

    Int1 to include only players

--- int1.sql
+++ int1.sql
@@ -5,6 +5,7 @@
 .width 20 6
 /*INTERROGACAO 1 - Top jogadores mais altos*/
 
-SELECT nome, altura
-FROM Pessoa
+SELECT Pessoa.nome as Nome_Jogador, Pessoa.altura
+FROM Jogador INNER JOIN Pessoa
+ON Jogador.idPessoa=Pessoa.idPessoa
 ORDER BY altura DESC LIMIT 10;
