3be9bb41757db8c03d58594a164c836f9673b270
Bernardo Barbosa <up201503477@fe.up.pt>
2017-05-23 22:43:58 +0100

    Update Povoar.sql, start int9

--- int8.sql
+++ int8.sql
@@ -2,6 +2,7 @@
 .headers on
 .nullvalue NULL
 
+.width 20 10
 /*INTERROGACAO 8 - Quantidade de jogadores para cada posicao */
 
 SELECT posicaoPref, count(posicaoPref) AS num
--- int9.sql
+++ int9.sql
@@ -2,4 +2,17 @@
 .headers on
 .nullvalue NULL
 
+.width 10 20
 /*INTERROGACAO 9 - Média de Idades por Equipa*/
+
+/*
+SELECT  ContratoJogador.*, AVG(strftime('%Y', 'now')-strftime ('%Y', Pessoa.dataNascimento)) as Idade
+FROM ContratoJogador, Pessoa
+WHERE idEquipa=4 AND idJogador = idPessoa;
+ */
+
+
+-- SELECT idEquipa as ID,nome as Nome FROM Equipa
+SELECT Equipa.idEquipa,Equipa.nome, AVG(strftime('%Y', 'now')-strftime ('%Y', Pessoa.dataNascimento)) as Média_Idades
+FROM Equipa,ContratoJogador, Pessoa
+WHERE idEquipa=4 AND idJogador = idPessoa;
