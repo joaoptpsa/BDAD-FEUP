7d8396f9d6321ff09c055ec772272858b058c090
João Sá <up201506252@fe.up.pt>
2017-05-23 19:18:03 +0100

    Create and Drop Trigger1

--- gatilho1_adiciona.sql
+++ gatilho1_adiciona.sql
@@ -1,5 +1,14 @@
 
 /* Dentro da relacao Jornada Para o mesmo idEpoca so podem haver 34 tuplos*/
 
-CREATE_TRIGGER IF NOT EXISTS Jornada_Insert
+CREATE TRIGGER IF NOT EXISTS NUM_ROUNDS
 BEFORE INSERT ON Jornada
+FOR EACH ROW --Already default
+WHEN (
+  SELECT count (Jornada.idEpoca)
+  FROM Jornada
+  WHERE (Jornada.idEpoca = new.idEpoca)
+  >= 34)
+BEGIN
+  SELECT RAISE(ABORT, 'So podem haver 34 jornadas numa Epoca');
+END;
