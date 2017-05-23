fdcecdd57cbcd5a34f915a2602a610dddfb0785d
epassos26 <epassos26@gmail.com>
2017-05-23 17:17:03 +0100

    Changed Epoca data

--- povoar.sql
+++ povoar.sql
@@ -2,7 +2,7 @@
 PRAGMA	foreign_keys	=	ON;
 
 --Epoca
-insert into Epoca (idEpoca, dataInicio, dataFim) values (1, '2016-08-13', '2017-04-03');
+insert into Epoca (idEpoca, dataInicio, dataFim) values (1, '2016-08-26', '2017-05-20');
 insert into Epoca (idEpoca, dataInicio, dataFim) values (2, '2017-08-13', '2018-04-03');
 insert into Epoca (idEpoca, dataInicio, dataFim) values (3, '2018-08-13', '2019-04-03');
 insert into Epoca (idEpoca, dataInicio, dataFim) values (4, '2019-08-13', '2020-04-03');
