33a21f7b7cc17861e6262dbd510adf2432bd6376
João Sá <up201506252@fe.up.pt>
2017-05-23 20:02:45 +0100

    Forgot a bracket

--- gatilho1_adiciona.sql
+++ gatilho1_adiciona.sql
@@ -1,14 +1,14 @@
-
 /* Dentro da relacao Jornada Para o mesmo idEpoca so podem haver 34 tuplos*/
 
 CREATE TRIGGER IF NOT EXISTS NUM_ROUNDS
 BEFORE INSERT ON Jornada
 FOR EACH ROW --Already default
 WHEN (
-  SELECT count (Jornada.idEpoca)
+  (SELECT count (Jornada.idEpoca)
   FROM Jornada
-  WHERE (Jornada.idEpoca = new.idEpoca)
-  >= 34)
+  WHERE Jornada.idEpoca = new.idEpoca)>=34)
 BEGIN
   SELECT RAISE(ABORT, 'So podem haver 34 jornadas numa Epoca');
 END;
+
+--INSERT INTO Jornada VALUES (35, '2017-05-25', '2017-06-01', 1);
