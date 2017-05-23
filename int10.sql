242c4125f958df8333d590e1624677a2186c8bda
João Sá <up201506252@fe.up.pt>
2017-05-23 13:03:13 +0100

    Fixed bug in int10.sql

--- int10.sql
+++ int10.sql
@@ -28,15 +28,36 @@ ON Pessoa.idPessoa=Jogador.idPessoa
 GROUP BY Pessoa.nome;
 */
 
-SELECT Pessoa.nome, Amarelos.num as Amarelos, Vermelhos.num as Vermelhos
+SELECT Pessoa.nome,
+CASE Amarelos.num
+WHEN Jogador.idPessoa = Amarelos.id THEN
+Amarelos.num
+ELSE
+0
+END Amarelos,
+CASE Vermelhos.num
+WHEN Jogador.idPessoa = Vermelhos.id THEN
+Vermelhos.num
+ELSE
+0
+END Vermelhos
 FROM Jogador, Pessoa,
 (SELECT Pessoa.idPessoa as id, count (Pessoa.idPessoa) as num
-  FROM Amarelo, Evento, Jogador, Pessoa
-  WHERE (Amarelo.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa AND Jogador.idPessoa = Pessoa.idPessoa)
+  FROM Amarelo INNER JOIN Evento
+  ON Amarelo.idEvento = Evento.idEvento
+  INNER JOIN Jogador
+  ON Jogador.idPessoa = Evento.idJogador
+  INNER JOIN Pessoa
+  ON Pessoa.idPessoa=Jogador.idPessoa
   GROUP BY Pessoa.idPessoa) Amarelos,
 (SELECT Pessoa.idPessoa as id, count (Pessoa.idPessoa) as num
-  FROM Vermelho, Evento, Jogador, Pessoa
-  WHERE (Vermelho.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa AND Jogador.idPessoa = Pessoa.idPessoa)
+  FROM Vermelho INNER JOIN Evento
+  ON Vermelho.idEvento = Evento.idEvento
+  INNER JOIN Jogador
+  ON Jogador.idPessoa = Evento.idJogador
+  INNER JOIN Pessoa
+  ON Pessoa.idPessoa=Jogador.idPessoa
   GROUP BY Pessoa.idPessoa) Vermelhos
 WHERE (Jogador.idPessoa=Pessoa.idPessoa AND (Jogador.idPessoa = Amarelos.id OR Jogador.idPessoa = Vermelhos.id))
-GROUP BY Pessoa.nome;
+GROUP BY Pessoa.idPessoa
+ORDER BY Vermelhos.num DESC;
