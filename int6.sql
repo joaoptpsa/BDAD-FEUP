7fef062fd91fe6b006aa33768b06761a79c3bc39
João Sá <up201506252@fe.up.pt>
2017-05-23 23:17:10 +0100

    int6 - League 2016/2017 standings

--- int6.sql
+++ int6.sql
@@ -2,15 +2,27 @@
 .headers on
 .nullvalue NULL
 
-/*INTERROGACAO 6 - TOP 10 Jogadores com mais minutos*/
+/*INTERROGACAO 6 - Classificacao Epoca 2016/2017*/
+--Sabemos que o idEpoca = 1
 
---Nao sabemos a duracao dos jogos e qualquer operacao de soma com null da null '''''''''''''''''WHAT DO?!?
---Solucoes:
---Considerar x tempo caso minutoSaida == null
---Adicionar campo duracao em cada Jogo
-SELECT Pessoa.nome, sum (Convocado.minutoSaida - Convocado.minutoEntrada) as tempoJogado
-FROM Convocado INNER JOIN Jogador
-ON Convocado.idJogador=Jogador.idPessoa
-INNER JOIN Pessoa
-ON Jogador.idPessoa=Pessoa.idPessoa
-ORDER BY tempoJogado DESC LIMIT 10;
+SELECT Equipa.nome, sum (
+CASE 
+WHEN ((Jogo.idEquipaCasa=Equipa.idEquipa) AND (Jogo.golosCasa-Jogo.golosFora)>0)
+THEN 3
+WHEN ((Jogo.idEquipaCasa=Equipa.idEquipa) AND (Jogo.golosCasa-Jogo.golosFora)=0)
+THEN 1
+WHEN ((Jogo.idEquipaCasa=Equipa.idEquipa) AND (Jogo.golosCasa-Jogo.golosFora)<0)
+THEN 0
+WHEN ((Jogo.idEquipaFora=Equipa.idEquipa) AND (Jogo.golosFora-Jogo.golosCasa)>0)
+THEN 3
+WHEN ((Jogo.idEquipaFora=Equipa.idEquipa) AND (Jogo.golosFora-Jogo.golosCasa)=0)
+THEN 1
+WHEN ((Jogo.idEquipaFora=Equipa.idEquipa) AND (Jogo.golosFora-Jogo.golosCasa)<0)
+THEN 0
+END )Pontos
+FROM Jogo INNER JOIN Jornada
+ON ((Jornada.idEpoca=1) AND (Jogo.idJornada=Jornada.idJornada))
+INNER JOIN Equipa
+ON (Jogo.idEquipaCasa=Equipa.idEquipa OR Jogo.idEquipaFora=Equipa.idEquipa)
+GROUP BY Equipa.idEquipa
+ORDER BY Pontos DESC;
