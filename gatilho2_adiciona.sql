ede7eec2a2fca8ac6c4302da619766c8dacb7003
João Sá <up201506252@fe.up.pt>
2017-05-24 20:13:13 +0100

    Trigger2 update

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -3,79 +3,19 @@
 CREATE TRIGGER IF NOT EXISTS Amarelo_Triggers
 BEFORE INSERT ON Amarelo
 FOR EACH ROW --Already default
-BEGIN
-  CASE
-  WHEN ((SELECT count (Evento.idEvento)
-  FROM Evento LEFT OUTER JOIN Amarelo
-  ON (Evento.idEvento = Amarelo.idEvento)
-  LEFT OUTER JOIN Vermelho
-  ON (Evento.idEvento = Vermelho.idEvento)
-  LEFT OUTER JOIN Golo
-  ON (Evento.idEvento = Golo.idEvento)
-  WHERE (Evento.idEvento = new.idEvento))>=1)
-  THEN SELECT RAISE(ABORT, 'Um evento só pode ser referenciado uma vez');
-
-  WHEN ((SELECT count (Jogador.idPessoa)
-  FROM Amarelo INNER JOIN Evento
-  ON (Amarelo.idEvento = Evento.idEvento)
-  INNER JOIN Jogo
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogo
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
-  INNER JOIN Jogador
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogador
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
-  GROUP BY (Jogador.idPessoa))>=1
-  AND
-  (SELECT count (Jogador.idPessoa)
-  FROM Vermelho INNER JOIN Evento
-  ON (Vermelho.idEvento = Evento.idEvento)
-  INNER JOIN Jogo
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogo
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
-  INNER JOIN Jogador
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogador
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
-  GROUP BY (Jogador.idPessoa))=0)
-  THEN INSERT INTO Vermelho (idEvento) values (new.idEvento);
-
-  WHEN ((SELECT count (Jogador.idPessoa)
-  FROM Amarelo INNER JOIN Evento
-  ON (Amarelo.idEvento = Evento.idEvento)
-  INNER JOIN Jogo
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogo
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
-  INNER JOIN Jogador
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogador
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
-  GROUP BY (Jogador.idPessoa))>=1
-  AND
-  (SELECT count (Jogador.idPessoa)
-  FROM Vermelho INNER JOIN Evento
-  ON (Vermelho.idEvento = Evento.idEvento)
-  INNER JOIN Jogo
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogo
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
-  INNER JOIN Jogador
-  ON ((SELECT Jogo.idJogo
-  FROM Evento INNER JOIN Jogador
-  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
-  GROUP BY (Jogador.idPessoa))>=1)
-  THEN SELECT RAISE(ABORT, 'O jogador já foi expulso deste jogo');
-  END
-END;
-
---Obter o numero de Amarelos no jogo para o jogador qual estamos a acrescentar um Amarelo
 /*
-SELECT count (Jogador.idPessoa)
-FROM Amarelo INNER JOIN Evento
-ON (Amarelo.idEvento = Evento.idEvento)
+WHEN ((SELECT count (Evento.idEvento)
+FROM Evento LEFT OUTER JOIN Amarelo
+ON (Evento.idEvento = Amarelo.idEvento)
+LEFT OUTER JOIN Vermelho
+ON (Evento.idEvento = Vermelho.idEvento)
+LEFT OUTER JOIN Golo
+ON (Evento.idEvento = Golo.idEvento)
+WHERE (Evento.idEvento = new.idEvento))>=1
+OR
+((SELECT count (Jogador.idPessoa)
+FROM Vermelho INNER JOIN Evento
+ON (Vermelho.idEvento = Evento.idEvento)
 INNER JOIN Jogo
 ON ((SELECT Jogo.idJogo
 FROM Evento INNER JOIN Jogo
@@ -84,17 +24,74 @@ INNER JOIN Jogador
 ON ((SELECT Jogo.idJogo
 FROM Evento INNER JOIN Jogador
 ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
-GROUP BY (Jogador.idPessoa);
-*/
-
---Obter o numero de vezes que um evento e referenciado por qualquer
-/*
-SELECT count (Evento.idEvento)
-FROM Evento LEFT OUTER JOIN Amarelo
-ON (Evento.idEvento = Amarelo.idEvento)
-LEFT OUTER JOIN Vermelho
-ON (Evento.idEvento = Vermelho.idEvento)
-LEFT OUTER JOIN Golo
-ON (Evento.idEvento = Golo.idEvento)
-WHERE (Evento.idEvento = new.idEvento)
-*/
+GROUP BY (Jogador.idPessoa))>=1))*/
+BEGIN
+  SELECT CASE
+  WHEN (
+    (SELECT count (Evento.idEvento)
+    FROM Evento LEFT OUTER JOIN Amarelo
+    ON (Evento.idEvento = Amarelo.idEvento)
+    LEFT OUTER JOIN Vermelho
+    ON (Evento.idEvento = Vermelho.idEvento)
+    LEFT OUTER JOIN Golo
+    ON (Evento.idEvento = Golo.idEvento)
+    WHERE (Evento.idEvento = new.idEvento))>=1)
+    THEN RAISE(ABORT, 'Um evento só pode ser referenciado uma vez')
+  WHEN (
+    ((SELECT count (Jogador.idPessoa)
+    FROM Amarelo INNER JOIN Evento
+    ON (Amarelo.idEvento = Evento.idEvento)
+    INNER JOIN Jogo
+    ON ((SELECT Jogo.idJogo
+    FROM Evento INNER JOIN Jogo
+    ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
+    INNER JOIN Jogador
+    ON ((SELECT Jogo.idJogo
+    FROM Evento INNER JOIN Jogador
+    ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
+    GROUP BY (Jogador.idPessoa))>=1)
+    AND
+    ((SELECT count (Jogador.idPessoa)
+    FROM Vermelho INNER JOIN Evento
+    ON (Vermelho.idEvento = Evento.idEvento)
+    INNER JOIN Jogo
+    ON ((SELECT Jogo.idJogo
+    FROM Evento INNER JOIN Jogo
+    ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
+    INNER JOIN Jogador
+    ON ((SELECT Jogo.idJogo
+    FROM Evento INNER JOIN Jogador
+    ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
+    GROUP BY (Jogador.idPessoa))>=1))
+  THEN RAISE(ABORT, 'O jogador já foi expulso deste jogo')
+  END;
+END;
+  /*
+  CASE
+  WHEN (((SELECT count (Jogador.idPessoa)
+  FROM Amarelo INNER JOIN Evento
+  ON (Amarelo.idEvento = Evento.idEvento)
+  INNER JOIN Jogo
+  ON ((SELECT Jogo.idJogo
+  FROM Evento INNER JOIN Jogo
+  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
+  INNER JOIN Jogador
+  ON ((SELECT Jogo.idJogo
+  FROM Evento INNER JOIN Jogador
+  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
+  GROUP BY (Jogador.idPessoa))>=1))
+  AND
+  (SELECT count (Jogador.idPessoa)
+  FROM Vermelho INNER JOIN Evento
+  ON (Vermelho.idEvento = Evento.idEvento)
+  INNER JOIN Jogo
+  ON ((SELECT Jogo.idJogo
+  FROM Evento INNER JOIN Jogo
+  ON (new.idEvento = Evento.idEvento AND Evento.idJogo = Jogo.idJogo))=Jogo.idJogo)
+  INNER JOIN Jogador
+  ON ((SELECT Jogo.idJogo
+  FROM Evento INNER JOIN Jogador
+  ON (new.idEvento = Evento.idEvento AND Evento.idJogador = Jogador.idPessoa))=Jogador.idPessoa)
+  GROUP BY (Jogador.idPessoa))>=0)
+  THEN (INSERT INTO Vermelho values (new.idEvento))
+  END;
--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,4 +1,6 @@
 
 INSERT INTO Amarelo values (5); --Rejeitado por ja haver algum evento com esse idEvento
-
---insert into Evento (idEvento, minuto, idEquipa, idJogador, idJogo) values (11, 45, 4, 400, 1);
+insert into Evento (idEvento, minuto, idEquipa, idJogador, idJogo) values (12, 60, 4, 264, 1); -- segundo amarelo
+insert into Evento (idEvento, minuto, idEquipa, idJogador, idJogo) values (13, 64, 4, 264, 1);
+INSERT INTO Vermelho values (12);
+INSERT INTO Amarelo values (13);
