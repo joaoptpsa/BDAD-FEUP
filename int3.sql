0329c448d119ef0c4d115aebca728741e4209f04
João Sá <up201506252@fe.up.pt>
2017-05-25 12:18:05 +0100

    Referees that gave out the most cards Season 16/17

--- int3.sql
+++ int3.sql
@@ -4,14 +4,44 @@
 
 .width 20 6
 
-/*INTERROGACAO 3 - Paises mais representados na liga*/
+/*INTERROGACAO 3 - Arbitros que deram mais cartoes 2016/2017*/
 
-SELECT Pais.nome, count(Pais.nome) as num
-FROM Jogador INNER JOIN Pessoa
-ON Jogador.idPessoa=Pessoa.idPessoa
-INNER JOIN Cidade
-ON Pessoa.idCidadeNasc = Cidade.idCidade
-INNER JOIN Pais
-ON Cidade.idPais=Pais.idPais
-GROUP BY Pais.nome
-ORDER BY num DESC;
+SELECT Pessoa.nome, Amarelos.num_Cartoes as Amarelos, Vermelhos.num_Cartoes as Vermelhos,Amarelos.num_Cartoes+Vermelhos.num_Cartoes as num_Cartoes
+FROM Jogo INNER JOIN Jornada
+ON ((Jornada.idEpoca=1) AND (Jogo.idJornada=Jornada.idJornada))
+INNER JOIN Arbitro
+ON (Jogo.idArbitro=Arbitro.idPessoa)
+INNER JOIN Pessoa
+ON (Arbitro.idPessoa=Pessoa.idPessoa),
+(SELECT Jogo.idJogo as idJogo, count (*) as num_Cartoes
+FROM Jogo INNER JOIN Evento
+ON (Jogo.idJogo=Evento.idJogo)
+INNER JOIN Amarelo
+ON (Evento.idEvento=Amarelo.idEvento)
+GROUP BY Jogo.idJogo) Amarelos,
+(SELECT Jogo.idJogo as idJogo, count (*) as num_Cartoes
+FROM Jogo INNER JOIN Evento
+ON (Jogo.idJogo=Evento.idJogo)
+INNER JOIN Vermelho
+ON (Evento.idEvento=Vermelho.idEvento)
+GROUP BY Jogo.idJogo) Vermelhos
+WHERE (Jogo.idJogo = Amarelos.idJogo OR Jogo.idJogo = Vermelhos.idJogo)
+GROUP BY Pessoa.idPessoa
+ORDER BY num_Cartoes DESC;
+/*
+--Cartoes Amarelos por idJogo
+SELECT Jogo.idJogo, count (*) as num_Cartoes
+FROM Jogo INNER JOIN Evento
+ON (Jogo.idJogo=Evento.idJogo)
+INNER JOIN Amarelo
+ON (Evento.idEvento=Amarelo.idEvento)
+GROUP BY Jogo.idJogo;
+
+--Cartoes Vermelhos pos idJogo
+SELECT Jogo.idJogo, count (*) as num_Cartoes
+FROM Jogo INNER JOIN Evento
+ON (Jogo.idJogo=Evento.idJogo)
+INNER JOIN Vermelho
+ON (Evento.idEvento=Vermelho.idEvento)
+GROUP BY Jogo.idJogo;
+*/
