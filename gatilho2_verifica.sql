9b23779d485b6e2e20978e1257599c968248453f
João Sá <up201506252@fe.up.pt>
2017-05-28 18:37:30 +0100

    Trigger tests updated

--- gatilho2_verifica.sql
+++ gatilho2_verifica.sql
@@ -1,6 +1,11 @@
 --Amarelo_Event_Refences_Limit
+SELECT * FROM Amarelo;
+
 INSERT INTO Amarelo values (5); --Rejeitado por ja haver algum evento com esse idEvento
 
 --Verifica Amarelo_Already_Expelled
-insert into Evento (idEvento, minuto, idEquipa, idJogador, idJogo) values (12, 91, 4, 4, 1); --Amarelo depois de ser expulso
-INSERT INTO Amarelo values (12);
+
+insert into Evento (idEvento, minuto, idEquipa, idJogador, idJogo) values (64, 81, 11, 313, 6); --Amarelo depois de ser expulso
+INSERT INTO Amarelo values (64);
+
+SELECT * FROM Amarelo;
--- gatilho3_verifica.sql
+++ gatilho3_verifica.sql
@@ -1,7 +1,6 @@
 SELECT * FROM Pessoa;
 
-INSERT INTO Pessoa VALUES (627, 'Edgar Passos', '2030-04-26', 170, 80, 176);
-INSERT INTO Pessoa VALUES (628, 'Bernardo CR7 Barbosa', '2030-04-02', 170, 80, 176);
-INSERT INTO Pessoa VALUES (629, 'João Sá', '1996-07-14', 170, 80, 176);-- Este vai acrescentar normal
+INSERT INTO Pessoa VALUES (570, 'Edgar Passos', '2030-04-26', 170, 80, 176);
+INSERT INTO Pessoa VALUES (571, 'Bernardo CR7 Barbosa', '2030-04-02', 170, 80, 176);
 
-SELECT * FROM Pessoa
+SELECT * FROM Pessoa;
