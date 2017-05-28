6ace3d62205701eb1ff6e125da1e0d724f751d03
João Sá <up201506252@fe.up.pt>
2017-05-28 18:24:35 +0100

    Best scorers for 2016/17 Season

--- int2.sql
+++ int2.sql
@@ -3,14 +3,18 @@
 .nullvalue NULL
 
 .width 30 6
-/*INTERROGACAO 2 - Melhores marcardores*/
+/*INTERROGACAO 2 - Melhores marcardores Época 2016/17*/
 
-SELECT Pessoa.nome, count(Pessoa.nome) as num
-FROM Golo INNER JOIN Evento
-ON Golo.idEvento = Evento.idEvento
+Select Pessoa.nome, count(Pessoa.idPessoa) as Golos_Marcados
+FROM Jogo INNER JOIN Jornada
+ON ((Jornada.idEpoca=1) AND (Jogo.idJornada=Jornada.idJornada))
+INNER JOIN Evento
+ON (Evento.idJogo=Jogo.idJogo)
+INNER JOIN Golo
+ON (Golo.idEvento=Evento.idEvento)
 INNER JOIN Jogador
-ON Jogador.idPessoa = Evento.idJogador
+ON Jogador.idPessoa=Evento.idJogador
 INNER JOIN Pessoa
 ON Pessoa.idPessoa=Jogador.idPessoa
-GROUP BY Pessoa.nome
-ORDER BY num DESC;
+GROUP BY Pessoa.idPessoa
+ORDER BY Golos_Marcados DESC;
